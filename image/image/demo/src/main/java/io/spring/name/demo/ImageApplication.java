26771a91b4d28bc4822520fd7d5cf6b3ea0ece42
Lukka.g <101813207+linkpillow@users.noreply.github.com>
2026-02-27 11:23:29 -0500

    Add files via upload

---
+++ image/image/demo/src/main/java/io/spring/name/demo/ImageApplication.java
@@ -0,0 +1,11 @@
+package io.spring.name.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ImageApplication.class, args);
+	}
+}
