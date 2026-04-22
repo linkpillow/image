b622ed76326bfd3e6f5f780e382d7778fd6fa281
Lukka.g <101813207+linkpillow@users.noreply.github.com>
2026-04-22 10:26:31 -0400

    Add files via upload

---
+++ src/main/java/io/spring/image/demo/application/ImageServiceImpl.java
@@ -0,0 +1,21 @@
+package io.spring.image.demo.application;
+
+import io.spring.image.demo.domain.entity.Image;
+import io.spring.image.demo.domain.service.ImageService;
+import io.spring.image.demo.infra.repository.ImageRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class ImageServiceImpl implements ImageService {
+    private final ImageRepository repository;
+
+    @Override
+    @Transactional
+    public Image save(Image image){
+        return null;
+    }
+}
